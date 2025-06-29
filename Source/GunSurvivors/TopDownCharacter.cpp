4c345bf007fbba6eb9252e9670da82600fa25d3c
Davide Ingargiola <topavide@gmail.com>
2025-06-29 23:00:26 +0200

    Add TopDownCharacter class Add PlayerCharacter blueprint

---
+++ Source/GunSurvivors/TopDownCharacter.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TopDownCharacter.h"
+
+// Sets default values
+ATopDownCharacter::ATopDownCharacter()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	SetRootComponent(CapsuleComp);
+
+	CharacterFlipbook = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("CharacterFlipbook"));
+	CharacterFlipbook->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ATopDownCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATopDownCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATopDownCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
