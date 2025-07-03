ac6f9af5cbdf209191b9a40c1a088c2ca25d6491
Davide Ingargiola <topavide@gmail.com>
2025-07-03 12:16:52 +0200

    Add Bullet class Add Bullet blueprint

---
+++ Source/GunSurvivors/Bullet.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Components/SphereComponent.h"
+#include "PaperSpriteComponent.h"
+
+#include "Bullet.generated.h"
+
+UCLASS()
+class GUNSURVIVORS_API ABullet : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABullet();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Collision")
+	USphereComponent* SphereComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Sprite")
+	UPaperSpriteComponent* SpriteComponent;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
